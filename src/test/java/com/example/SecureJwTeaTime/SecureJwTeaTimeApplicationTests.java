4cc3b0972049b35f6ccfad8dd63e5eccbafd52a7
Laila <175353500+lvanrooijen@users.noreply.github.com>
2025-08-22 21:06:10 +0200

    Initial commit

---
+++ src/test/java/com/example/SecureJwTeaTime/SecureJwTeaTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecureJwTeaTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureJwTeaTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
